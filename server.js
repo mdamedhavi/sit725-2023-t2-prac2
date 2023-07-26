bb3e377f4d3002628b0fbb2d5d38418c91c44b30
Manamperige Dewni <s223074638@deakin.edu.au>
2023-07-26 19:24:48 +1000

    commit1`

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+const res = require("express/lib/response");
+var app = express()
+var port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/',(req,res)=>{
+    res.render('index.html')
+});
+
+app.post('',(req,res)=>
+{});
+
+app.use('/addTwoNumbers', (req,res)=>{
+    var n1= req.query.n1;
+    var n2= req.query.n2;
+    var result= addNumbers(n1,n2);
+    res.json({statuscode:200,data:result,message:"Success"});
+});
+ 
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
